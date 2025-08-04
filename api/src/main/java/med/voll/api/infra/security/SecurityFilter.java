84239f7c8d58c617dda34aa61d5d87d5b77b6e57
GermanEduardoVega <german.vega@alumnos.frm.utn.edu.ar>
2025-08-03 21:14:43 -0300

    Testeando el control de acceso

--- api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -30,6 +30,7 @@ public class SecurityFilter extends OncePerRequestFilter {
 
             var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
+            System.out.println("Usuario" + subject + " autenticado");
         }
 
         filterChain.doFilter(request, response);
