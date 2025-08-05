bff827d60d60ea554e75c54d5b0a52f55dde6811
GermanEduardoVega <german.vega@alumnos.frm.utn.edu.ar>
2025-08-05 12:29:36 -0300

    gestionando más errores posibles | errores en la lectura de mensajes HTTP, con la autenticación, de acceso denegado y de error 500 con un mensaje localizado

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -10,6 +10,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -52,7 +54,6 @@ public class MedicoController {
             medico.eliminarInformacion();
             return ResponseEntity.noContent().build();
         }
-
 
     @GetMapping("/{id}")
     public ResponseEntity detallar(@PathVariable Long id){
